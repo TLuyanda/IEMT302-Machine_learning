3fe95b4f29a236ebf6ea1c1712ca131b50d0df3b
Luyanda Tyaleni <s224290568@mandela.ac.za>
2024-08-07 17:09:15 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Machine Learning Example
+
+This repository demonstrates a simple linear regression model using Python and Scikit-learn. The purpose of this example is to provide a basic understanding of how a linear regression model works.
+
+
---
+++ simple_linear_regression.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.linear_model import LinearRegression 
+
+# Generate some example data
+# X: feature (independent variable), y: target (dependent variable)
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([1, 2, 1.3, 3.75, 2.25])
+
+# Create and train the model
+model = LinearRegression()
+model.fit(X, y)
+
+# Make predictions
+y_pred = model.predict(X)
+
+# Plot the results
+plt.scatter(X, y, color='blue', label='Original data')
+plt.plot(X, y_pred, color='red', label='Fitted line')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+# Print model parameters
+print(f'Coefficient: {model.coef_[0]}')
+print(f'Intercept: {model.intercept_}')
